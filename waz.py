7fea766de89c154d25502f5b4b948f681d49d9b1
Przemysław Gembalczyk <gembuspol@interia.pl>
2023-03-27 14:09:50 +0200

    klasa
    Signed-off-by: Przemysław Gembalczyk <gembuspol@interia.pl>

--- waz.py
+++ waz.py
@@ -1,6 +1,11 @@
 #pobranie biblioteki pygame
 import random
 import pygame
+#import kodu z klasą wąż
+import wazKlasa
+
+#tworzenie obiektu wąż
+obiektWaz1=wazKlasa.WazKlas()
 
 #utworzenie funkcji waz
 def waz():
@@ -13,24 +18,25 @@ def waz():
     #tworzymy zmienną, która przechowuje informacje czy gra jest uruchomiona
     run=True
    
-    #pozycje startowe węża
+    """ #pozycje startowe węża
     zmiennaX=300
     zmiennaY=300
     #stworzenie listy z pozycjami dla weza
     pozycjeWaz=[(zmiennaX,zmiennaY)]
     #dlugosc weza
-    dlugoscWaz=1
+    dlugoscWaz=1 """
     #pozycja startowa jabłka
     jablkoX=random.randint(0,19)*30
     jablkoY=random.randint(0,19)*30
-    #liczba punktów
+    punkty=0
+    """#liczba punktów
     punkty=0
     #kierunek początkowy węża
     #[1,0] - ruch w prawo
     #[0,1] - ruch w dół
     #[-1,0] - ruch w lewo
     #[0,-1] - ruch w górę
-    kierunek=[1,0]
+    kierunek=[1,0]"""
     #pętla while sprawdza czy warunek w zmiennej run jest prawdziwy, jak jest nieprawdziwy kończy swoje działanie
     while(run):
         #Wypełnienie okna kolorem
@@ -45,18 +51,19 @@ def waz():
                 #obsługa zdarzeń klawiatury i zmiana pozycji węża
             elif zdarzenia.type==pygame.KEYDOWN:
                 if zdarzenia.key==pygame.K_RIGHT:
-                    kierunek=[1,0]
+                    obiektWaz1.setDirection([1,0])
                 elif zdarzenia.key==pygame.K_LEFT:
-                    kierunek=[-1,0]
+                    obiektWaz1.setDirection([-1,0])
                 elif zdarzenia.key==pygame.K_UP:
-                    kierunek=[0,-1]
+                    obiektWaz1.setDirection([0,-1])
                 elif zdarzenia.key==pygame.K_DOWN:
-                    kierunek=[0,1]
+                    obiektWaz1.setDirection([0,1])
         #ustalenie nowej pozycji węża
-        zmiennaX=zmiennaX+kierunek[0]*30;
-        zmiennaY=zmiennaY+kierunek[1]*30;
+        """zmiennaX=zmiennaX+kierunek[0]*30;
+        zmiennaY=zmiennaY+kierunek[1]*30;"""
+        obiektWaz1.snakeMove()
                 
-        #sprawdzanie krawędzi
+        """#sprawdzanie krawędzi
         if zmiennaX>=600:
             zmiennaX=0
         if zmiennaX<0:
@@ -67,9 +74,11 @@ def waz():
             zmiennaY=600-30
         #dodanie nowej pozycji wężą do listy
         pozycjeWaz.append((zmiennaX,zmiennaY))
-        
+        """
+        #pozycja głowy weza
+        glowa=obiektWaz1.getHeadPosition()
         #zjadanie jabłka
-        if zmiennaX==jablkoX and zmiennaY == jablkoY:
+        if glowa[0]==jablkoX and glowa[1] == jablkoY:
             jablkoX=random.randint(0,19)*30
             jablkoY=random.randint(0,19)*30
             punkty+=1 #punkty=punkty+1
@@ -78,7 +87,8 @@ def waz():
         #rysowanie jabłka
         pygame.draw.circle(oknoGry,(255,0,0),(jablkoX+15,jablkoY+15),15)
         #rysowanie węża
-        #sprawdzamy długość węża
+        obiektWaz1.snakeDraw(oknoGry)
+        """#sprawdzamy długość węża
         if len(pozycjeWaz)>dlugoscWaz:
             del pozycjeWaz[0]
         #pętla rysująca węża
@@ -86,7 +96,7 @@ def waz():
         #definiujemy kształ węża
             ksztaltWaz=pygame.Rect((poz[0],poz[1]),(30,30))
             #dodanie kształtu do okienka
-            pygame.draw.rect(oknoGry,(100,100,100),ksztaltWaz)
+            pygame.draw.rect(oknoGry,(100,100,100),ksztaltWaz)"""
         #napisy na ekranie
         czcionka=pygame.font.SysFont('arial',25)
         tekst=czcionka.render("Zdobyłeś punkty: {0}".format(punkty),1,(51,51,255))
